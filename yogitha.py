61405bc00364792f182aa9f91a685121caba3a33
yogithamb <yogithamb5@gmail.com>
2026-07-18 19:19:26 +0530

    Add files via upload

---
+++ yogitha.py
@@ -0,0 +1,11 @@
+# Python Variables Example
+
+name = "Yogitha"
+age = 18
+height = 5.4
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
