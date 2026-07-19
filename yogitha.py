28f39a135b3e52181e2ad3cacbea309d69fbdb92
yogithamb <yogithamb5@gmail.com>
2026-07-19 23:01:44 +0530

    Add files via upload

--- yogitha.py
+++ yogitha.py
@@ -1,11 +1,3 @@
-# Python Variables Example
-
-name = "Yogitha"
-age = 18
-height = 5.4
-is_student = True
-
-print("Name:", name)
-print("Age:", age)
-print("Height:", height)
-print("Student:", is_student)
+Enter first number: 10
+Enter second number: 5
+Choose operation (+, -, *, /): +
